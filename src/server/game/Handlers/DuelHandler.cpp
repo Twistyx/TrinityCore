459132ce12b7d913521e3c0d15bb9c19c64d08f1
Twistyx <ruben.macchiato@gmail.com>
2013-12-16 00:49:06 +0100

    Update DuelHandler.cpp

--- src/server/game/Handlers/DuelHandler.cpp
+++ src/server/game/Handlers/DuelHandler.cpp
@@ -51,6 +51,12 @@ void WorldSession::HandleDuelAcceptedOpcode(WorldPacket& recvPacket)
 
     player->SendDuelCountdown(3000);
     plTarget->SendDuelCountdown(3000);
+    pl->RemoveAllSpellCooldown();
+    plTarget->RemoveAllSpellCooldown();
+    pl->SetHealth(pl->GetMaxHealth());
+    pl->SetPower(POWER_MANA, pl->GetMaxPower(POWER_MANA));
+    plTarget->SetHealth(plTarget->GetMaxHealth());
+    plTarget->SetPower(POWER_MANA,  plTarget->GetMaxPower(POWER_MANA));
 }
 
 void WorldSession::HandleDuelCancelledOpcode(WorldPacket& recvPacket)
