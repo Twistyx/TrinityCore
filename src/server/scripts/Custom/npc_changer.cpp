a5605618d1370cd1977a9a08bb04a840f57061da
kigiri <le.mikmac@gmail.com>
2014-03-11 02:44:10 +0100

    [Custom] Rewrite of npc changer.

--- src/server/scripts/Custom/npc_changer.cpp
+++ src/server/scripts/Custom/npc_changer.cpp
@@ -1,68 +1,64 @@
 #include "ScriptPCH.h"
- 
-enum  defines
-{
- faction_token = 100, // Faction Change Token
- race_token = 100 // Race Change Token
-};
- 
+#include "Chat.h"
+#include "Language.h"
+
 class npc_changer : public CreatureScript
 {
-        public:
-                npc_changer() : CreatureScript("npc_changer"){}
+public:
+    npc_changer() : CreatureScript("npc_changer"){ }
  
-                bool OnGossipHello(Player * pPlayer, Creature * pCreature)
-                {
-                       
-                       
-                        pPlayer->ADD_GOSSIP_ITEM(4, "Change My Race ", GOSSIP_SENDER_MAIN, 0);
-                    pPlayer->ADD_GOSSIP_ITEM(4, "Change My Faction", GOSSIP_SENDER_MAIN, 1);
-                        pPlayer->PlayerTalkClass->SendGossipMenu(9425, pCreature->GetGUID());
-                        return true;
-                }
+    bool OnGossipHello(Player *player, Creature * m_creature)
+    {
+        player->ADD_GOSSIP_ITEM(4, "Change My Race ", GOSSIP_SENDER_MAIN, 0);
+        player->ADD_GOSSIP_ITEM(4, "Change My Faction", GOSSIP_SENDER_MAIN, 1);
+        player->ADD_GOSSIP_ITEM(4, "Customize me !", GOSSIP_SENDER_MAIN, 2);
+        player->ADD_GOSSIP_ITEM(4, "Reset my talents, I have no talents anyway !", GOSSIP_SENDER_MAIN, 3);
+        player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, "Nevermind..", GOSSIP_SENDER_MAIN, 1000);
+        player->SEND_GOSSIP_MENU(1, m_creature->GetGUID());
+        return true;
+    }
  
-                bool OnGossipSelect(Player * Player, Creature * Creature, uint32 /*uiSender*/, uint32 uiAction)
-                {
-                        if(!Player)
-                                return true;
- 
-                        switch(uiAction)
-                        {
-                                case 0:
-                                        if(Player->HasItemCount(race_token, 1))
-                                        {
-                                                Player->DestroyItemCount(race_token, 1, true, false);
-                                                Player->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
-                                                Player->GetSession()->SendNotification("You need to relog, to change your race!");
-                                                Player->PlayerTalkClass->SendCloseGossip();
-                                        }
-                                        else
-                                        {
-                                                Player->GetSession()->SendNotification("You need atleast one race change token!");
-                                                Player->PlayerTalkClass->SendCloseGossip();
-                                        }
-                                        break;
-                                case 1:
-                                        if(Player->HasItemCount(faction_token, 1))
-                                        {
-                                                Player->DestroyItemCount(faction_token, 1, true, false);
-                                                Player->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-                                                Player->GetSession()->SendNotification("You need to relog, to change your faction!");
-                                                Player->PlayerTalkClass->SendCloseGossip();
-                                        }
-                                        else
-                                        {
-                                                Player->GetSession()->SendNotification("You need atleast one faction change token!");
-                                                Player->PlayerTalkClass->SendCloseGossip();
-                                        }
-                                        break;
-                        }
-                        return true;
-                }
- 
+    bool OnGossipSelect(Player *player, Creature * m_creature, uint32 /*uiSender*/, uint32 uiAction)
+    {
+        (void)m_creature;
+        if(!player)
+            return true;
+
+        if(uiAction == 0)
+        {
+            player->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
+            CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '128' WHERE guid = %u", player->GetGUID());
+            player->GetSession()->SendNotification("You have to relog, to change your race!");
+        }
+        else if (uiAction == 1)
+        {
+            player->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
+            CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '64' WHERE guid = %u", player->GetGUID());
+            player->GetSession()->SendNotification("You have to relog, to change your faction!");
+        }
+        else if (uiAction == 2)
+        {
+            PreparedStatement* stmt = CharacterDatabase.GetPreparedStatement(CHAR_UPD_ADD_AT_LOGIN_FLAG);
+            stmt->setUInt16(0, uint16(AT_LOGIN_CUSTOMIZE));
+            player->GetSession()->SendNotification("Yey Customize !!");
+            player->SetAtLoginFlag(AT_LOGIN_CUSTOMIZE);
+            stmt->setUInt32(1, player->GetGUIDLow());
+            CharacterDatabase.Execute(stmt);
+        }
+        else if (uiAction == 3)
+        {
+            player->resetTalents(true);
+            player->SendTalentsInfoData(false);
+            ChatHandler(player->GetSession()).SendSysMessage(LANG_RESET_TALENTS);
+            return true;
+        }
+        else
+            player->PlayerTalkClass->SendCloseGossip();
+        return true;
+    }
 };
  
 void AddSC_npc_changer()
 {
-        new npc_changer();
+    new npc_changer();
 }
