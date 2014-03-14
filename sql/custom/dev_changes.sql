2542c1f449d18f80846c7bc265730e2020479a32
kigiri <le.mikmac@gmail.com>
2014-03-14 23:05:06 +0100

    [DB] Missing Spells

--- sql/custom/dev_changes.sql
+++ sql/custom/dev_changes.sql
@@ -5,4 +5,8 @@ UPDATE `item_template`  SET bonding=1 WHERE `name` LIKE '%Glyph of%' AND `class`
 INSERT INTO `playercreateinfo_item` (`class`, `itemid`) VALUES ('64', '46978');
 
 -- Added Taunt to warriors and maul to druids (base spell)
-INSERT INTO `playercreateinfo_spell` (`classmask`, `Spell`, `Note`) VALUES ('1', '355', 'Taunt'), ('1024', '6808', 'Maul');
+DELETE FROM playercreateinfo_spell WHERE Spell=335;
+DELETE FROM playercreateinfo_spell WHERE Spell=6808;
+DELETE FROM playercreateinfo_spell WHERE Spell=50769;
+INSERT INTO `playercreateinfo_spell` (`classmask`, `Spell`, `Note`) VALUES ('1', '355', 'Taunt'), ('1024', '6808', 'Maul'), ('1024', '50769', 'Revive');
+
