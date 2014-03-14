ea0a0adc28fbe3e417910e8a873e82d671eb9249
kigiri <le.mikmac@gmail.com>
2014-03-14 19:33:41 +0100

    [DB] Added missing starting spells

--- sql/custom/dev_changes.sql
+++ sql/custom/dev_changes.sql
@@ -3,3 +3,6 @@ UPDATE `item_template`  SET bonding=1 WHERE `name` LIKE '%Glyph of%' AND `class`
 
 -- Give Totem of the Earthen Ring on character creation for shamans
 INSERT INTO `playercreateinfo_item` (`class`, `itemid`) VALUES ('64', '46978');
+
+-- Added Taunt to warriors and maul to druids (base spell)
+INSERT INTO `playercreateinfo_spell` (`classmask`, `Spell`, `Note`) VALUES ('1', '355', 'Taunt'), ('1024', '6808', 'Maul');
