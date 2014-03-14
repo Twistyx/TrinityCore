36b03b9c3afc10e493b5d61e16d8518a1530de1c
kigiri <le.mikmac@gmail.com>
2014-03-14 10:44:17 +0100

    [DB] Add Totem of the Earthen Ring on character creation for shamans.

--- sql/custom/dev_changes.sql
+++ sql/custom/dev_changes.sql
@@ -1,2 +1,5 @@
 -- Make all glyphs BoP
 UPDATE `item_template`  SET bonding=1 WHERE `name` LIKE '%Glyph of%' AND `class` = '16';
+
+-- Give Totem of the Earthen Ring on character creation for shamans
+INSERT INTO `playercreateinfo_item` (`class`, `itemid`) VALUES ('64', '46978');
