5e3a3b919a360d4642ca5df13dd7d038dd2f5b79
kigiri <le.mikmac@gmail.com>
2014-03-20 14:29:31 +0100

    [Custom] Faction failure debug.

--- src/server/scripts/Custom/npc_changer.cpp
+++ src/server/scripts/Custom/npc_changer.cpp
@@ -1,6 +1,7 @@
 #include "ScriptPCH.h"
 #include "Chat.h"
 #include "Language.h"
+#include "ReputationMgr.h"
 
 class npc_changer : public CreatureScript
 {
@@ -12,6 +13,7 @@ public:
         player->ADD_GOSSIP_ITEM(4, "Change My Race ", GOSSIP_SENDER_MAIN, 0);
         player->ADD_GOSSIP_ITEM(4, "Change My Faction", GOSSIP_SENDER_MAIN, 1);
         player->ADD_GOSSIP_ITEM(4, "Customize me !", GOSSIP_SENDER_MAIN, 2);
+        player->ADD_GOSSIP_ITEM(4, "Debug my faction (don't use if not bug or you will loose some)", GOSSIP_SENDER_MAIN, 2);
         player->ADD_GOSSIP_ITEM(GOSSIP_ICON_DOT, "Nevermind..", GOSSIP_SENDER_MAIN, 1000);
         player->SEND_GOSSIP_MENU(1, m_creature->GetGUID());
         return true;
@@ -43,6 +45,31 @@ public:
             player->SetAtLoginFlag(AT_LOGIN_CUSTOMIZE);
             stmt->setUInt32(1, player->GetGUIDLow());
             CharacterDatabase.Execute(stmt);
+        }
+        else if (uiAction == 3)
+        {
+            if (player->GetTeam() == ALLIANCE)
+            {
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(72), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(47), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(69), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(930), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(730), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(978), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(54), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(946), 50, false);
+            }
+            else
+            {
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(76), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(68), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(81), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(911), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(729), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(941), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(530), 50, false);
+                player->GetReputationMgr().SetOneFactionReputation(sFactionStore.LookupEntry(947), 50, false);
+            }
         }
         else
             player->PlayerTalkClass->SendCloseGossip();
