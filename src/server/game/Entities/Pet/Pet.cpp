ca7c33b62cc9ec8d1ec6300895c7fe2cc7a3cbda
kigiri <le.mikmac@gmail.com>
2014-04-03 02:18:40 +0200

    [Custom] No moar sadness for pets, enjoy a new world filled by joy and ponies !

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -673,6 +673,7 @@ void Creature::Regenerate(Powers power)
 
 void Pet::LoseHappiness()
 {
+    return; // let's ignore this boring mechanic.
     uint32 curValue = GetPower(POWER_HAPPINESS);
     if (curValue <= 0)
         return;
