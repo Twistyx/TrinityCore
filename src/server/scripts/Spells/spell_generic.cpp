ab890e3f4726bcb597e65ba120fcea0a2fd928c0
kigiri <le.mikmac@gmail.com>
2014-04-22 19:54:59 +0200

    Silence a warining on compile

--- src/server/scripts/Spells/spell_generic.cpp
+++ src/server/scripts/Spells/spell_generic.cpp
@@ -829,10 +829,12 @@ class spell_gen_clone_weapon_aura : public SpellScriptLoader
                         if (Player* player = caster->ToPlayer())
                         {
                             if (Item* offItem = player->GetItemByPos(INVENTORY_SLOT_BAG_0, EQUIPMENT_SLOT_OFFHAND))
+                            {
                                 if (uint32 entry = sTransmogrification->GetFakeEntry(offItem->GetGUID()))
                                     target->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, entry);
                                 else
                                     target->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, offItem->GetEntry());
+                            }
                         }
                         else
                             target->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1, caster->GetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 1));
@@ -845,10 +847,12 @@ class spell_gen_clone_weapon_aura : public SpellScriptLoader
                         if (Player* player = caster->ToPlayer())
                         {
                             if (Item* rangedItem = player->GetItemByPos(INVENTORY_SLOT_BAG_0, EQUIPMENT_SLOT_RANGED))
+                            {
                                 if (uint32 entry = sTransmogrification->GetFakeEntry(rangedItem->GetGUID()))
                                     target->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2, entry);
                                 else
                                     target->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2, rangedItem->GetEntry());
+                            }
                         }
                         else
                             target->SetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2, caster->GetUInt32Value(UNIT_VIRTUAL_ITEM_SLOT_ID + 2));
