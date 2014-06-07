85da98ede78b6ac1c35c2c52d1dbddb50b2d4823
kigiri <le.mikmac@gmail.com>
2014-06-07 22:36:52 +0200

    Revert "Added only duel in Spirit Rise (line 4252)"
    This reverts commit 4d9d3423d7559f0c1d1b1229db4b8a97fe8eca83.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -4252,7 +4252,6 @@ void Spell::EffectDuel(SpellEffIndex effIndex)
     // Players can only fight a duel in zones with this flag
     AreaTableEntry const* casterAreaEntry = GetAreaEntryByAreaID(caster->GetAreaId());
     if (casterAreaEntry && !(casterAreaEntry->flags & AREA_FLAG_ALLOW_DUELS))
-        if(caster->GetAreaId() == 1640)
     {
         SendCastResult(SPELL_FAILED_NO_DUELING);            // Dueling isn't allowed here
         return;
@@ -4260,7 +4259,6 @@ void Spell::EffectDuel(SpellEffIndex effIndex)
 
     AreaTableEntry const* targetAreaEntry = GetAreaEntryByAreaID(target->GetAreaId());
     if (targetAreaEntry && !(targetAreaEntry->flags & AREA_FLAG_ALLOW_DUELS))
-        if(caster->GetAreaId() == 1640)
     {
         SendCastResult(SPELL_FAILED_NO_DUELING);            // Dueling isn't allowed here
         return;
